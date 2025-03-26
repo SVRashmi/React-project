83d28047bcef27eb0b3cd30929c0c32409a81089
Rashmi V <rashmi.v@impelsys.com>
2025-03-26 15:20:40 +0530

    Fixed routing issue with HasRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import './index.css';
